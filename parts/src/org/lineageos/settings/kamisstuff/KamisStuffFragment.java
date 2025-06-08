9d45359ffd66a9253c5edc0c29ae2bd442599529
kamikaonashi <thorschi93@gmx.at>
2025-06-08 12:24:09 +0200

    stone: parts: add Kamis Stuff and shift some settings there

---
+++ parts/src/org/lineageos/settings/kamisstuff/KamisStuffFragment.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2025 KamiKaonashi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.kamisstuff;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import org.lineageos.settings.R;
+import org.lineageos.settings.corecontrol.CoreControlActivity;
+import org.lineageos.settings.fastcharge.FastChargeActivity;
+import org.lineageos.settings.zram.ZramActivity;
+
+public class KamisStuffFragment extends PreferenceFragment {
+
+    private static final String KEY_FAST_CHARGE = "fast_charge";
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.kamis_stuff_settings, rootKey);
+
+        // Fast Charge preference
+        Preference fastChargePref = findPreference(KEY_FAST_CHARGE);
+        if (fastChargePref != null) {
+            fastChargePref.setOnPreferenceClickListener(preference -> {
+                Intent intent = new Intent(getActivity(), FastChargeActivity.class);
+                startActivity(intent);
+                return true;
+            });
+        }
+    }
+}
